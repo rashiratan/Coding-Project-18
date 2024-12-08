b3728d657af7f375c20fab49318d32c6d1bc07a0
Rashi Ratan <rashiiratan@gmail.com>
2024-12-08 10:43:28 -0500

    Fetch chart data from JSON file

--- react-chart-app/src/App.jsx
+++ react-chart-app/src/App.jsx
@@ -4,8 +4,15 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [chartData, setChartData] = useState(null);
 
+  useEffect(() => {
+    fetch('/financial_data.json')
+    .then((response) => response.json())
+    .then((data) => setChartData(data));
+  }, []);
+
+  
   return (
     <>
       <div>
