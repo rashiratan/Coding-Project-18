28ed4b947b2f623b4d4ff47dbca68d27c7cf5bb1
Rashi Ratan <rashiiratan@gmail.com>
2024-12-08 10:59:57 -0500

    Render all charts in the App component

--- react-chart-app/src/App.jsx
+++ react-chart-app/src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
+import BarChart from './Components/BarChart';
+import LineChart from './Components/LineChart';
+import ScatterChart from './Components/ScatterChart';
+import BubbleChart from './Components/BubbleChart';
 import './App.css'
 
 function App() {
@@ -12,31 +15,23 @@ function App() {
     .then((data) => setChartData(data));
   }, []);
 
+  if (!chartData) {
+    return <div>Loading...</div>;
+  }
+
   
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+return (
+    <div style={{ textAlign: 'center' }}>
+    <h1>Dynamic Charts with React and Chart.js</h1>
+    <BarChart data={chartData} />
+    <br />
+    <LineChart data={chartData}/>
+    <br />
+    <ScatterChart data={chartData}/>
+    <br />
+    <BubbleChart data={chartData}/>
+    </div>
+)
 }
 
 export default App
